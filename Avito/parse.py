e754344da2a0ee7b25c39231a930daae6a697364
Oxinhagg <dim.makhortoff@gmail.com>
2021-10-27 12:38:27 +0300

    repair parse

--- Avito/parse.py
+++ Avito/parse.py
@@ -1,17 +1,19 @@
 from bs4 import BeautifulSoup
 
 def parse_page(page):
-    ad = {}
+    object = {}
+    object_list = []
 
     soap = BeautifulSoup(page, 'html.parser')
     item_list = soap.find('div', {'class' : 'items-items-kAJAg'})
     items = item_list.find_all('div', {'data-marker' : 'item'})
     for item in items:
-        ad['id'] = item.get('data-item-id')
-        ad['description'] = item.find('meta', {'itemprop' : 'description'}).get('content')
-        ad['link'] = item.find('div', {'class' : 'iva-item-titleStep-_CxvN'}).find('a').get('href')
+        object['id'] = item.get('data-item-id')
+        object['description'] = item.find('meta', {'itemprop' : 'description'}).get('content')
+        object['link'] = item.find('div', {'class' : 'iva-item-titleStep-_CxvN'}).find('a').get('href')
         priceStep = item.find('div', {'class' : 'iva-item-priceStep-QN8Kl'})
-        ad['priceCurrency'] = priceStep.find('meta', {'itemprop' : 'priceCurrency'}).get('content')
-        ad['price'] = priceStep.find('meta', {'itemprop' : 'price'}).get('content')
-    return ad
+        object['priceCurrency'] = priceStep.find('meta', {'itemprop' : 'priceCurrency'}).get('content')
+        object['price'] = priceStep.find('meta', {'itemprop' : 'price'}).get('content')
+        object_list.append(object)
+    return object_list
         
--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from Avito.config import get_url_list
 from Avito.parse import parse_page
 
 def main():
-    obj_list = []
     url_list = get_url_list()
     for url in url_list:
         page = get_html(url)
-        obj_list.append(parse_page(page))
+        obj_list = parse_page(page)
     print(obj_list)
 
 if __name__ == '__main__':
